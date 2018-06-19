f71e551c234854158b3db4f786d95dcbd160dea0
erisdinh <erisdinh@gmail.com>
2018-06-19 16:07:48 +0700

    Bus
    Add Bus class (constructors)

---
+++ BusManagementSystem/src/quynh/Bus.java
@@ -0,0 +1,41 @@
+package quynh;
+
+public class Bus {
+
+    private int busNumber;
+    private String busLicensePlate;
+    private String busType;
+    private int numOfSeat;
+
+    public int getBusNumber() {
+        return busNumber;
+    }
+
+    public void setBusNumber(int busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public String getBusLicensePlate() {
+        return busLicensePlate;
+    }
+
+    public void setBusLicensePlate(String busLicensePlate) {
+        this.busLicensePlate = busLicensePlate;
+    }
+
+    public String getBusType() {
+        return busType;
+    }
+
+    public void setBusType(String busType) {
+        this.busType = busType;
+    }
+
+    public int getNumOfSeat() {
+        return numOfSeat;
+    }
+
+    public void setNumOfSeat(int numOfSeat) {
+        this.numOfSeat = numOfSeat;
+    }
+}
