61bd9f2975c4a2626c2c6a612fece05e9b8bc097
erisdinh <erisdinh@gmail.com>
2018-07-05 13:30:00 +0700

    Login Form
    Add Sheridan logo
    Complete Login Form layout
    Add Login Form Controller

--- BusManagementSystem/src/quynh/ConnectSQLServer.java
+++ BusManagementSystem/src/quynh/ConnectSQLServer.java
@@ -12,30 +12,30 @@ public class ConnectSQLServer {
     private static String USER_NAME = "sa";
     private static String PASSWORD = "12345678";
     
-    public static void main(String args[]) {
-        try {
-            
-            // Connect to BusManagementDatabase
-            Connection connection = getConnection(DTB_URL,USER_NAME, PASSWORD);
-            
-            // Create statement
-            Statement statement = connection.createStatement();
-            
-            // Get data from tables
-            //ResultSet resultSet = statement.executeQuery("select * from "+ tableName);
-            
-            // Show data
-//            while(resultSet.next()){
-//                System.out.println(resultSet.getString());
-//            }
-
-            // Close connection
-            connection.close();
-           
-        } catch (Exception e) {
-            System.out.println("Connection Failure!");
-        }
-    }
+//    public static void main(String args[]) {
+//        try {
+//            
+//            // Connect to BusManagementDatabase
+//            Connection connection = getConnection(DTB_URL,USER_NAME, PASSWORD);
+//            
+//            // Create statement
+//            Statement statement = connection.createStatement();
+//            
+//            // Get data from tables
+//            //ResultSet resultSet = statement.executeQuery("select * from "+ tableName);
+//            
+//            // Show data
+////            while(resultSet.next()){
+////                System.out.println(resultSet.getString());
+////            }
+//
+//            // Close connection
+//            connection.close();
+//           
+//        } catch (Exception e) {
+//            System.out.println("Connection Failure!");
+//        }
+//    }
     
     public static Connection getConnection(String dbURL, String userName, String password) {
         Connection connection = null;
--- BusManagementSystem/src/quynh/LoginFormController.java
+++ BusManagementSystem/src/quynh/LoginFormController.java
@@ -5,9 +5,30 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class LoginFormController implements Initializable {
+
+    @FXML
+    private ToggleGroup accountType;
+    @FXML
+    private TextField textfieldID;
+    @FXML
+    private TextField textfieldPass;
+    @FXML
+    private RadioButton radioAdmin;
+    @FXML
+    private RadioButton radioUser;
+    @FXML
+    private Button buttonCancel;
+    @FXML
+    private Button buttonSignIn;
+    @FXML
+    private Label labelConnection;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
