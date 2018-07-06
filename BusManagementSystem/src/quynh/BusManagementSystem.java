e082da675da0d9987d3bc02bfa3f6ca0dea90e23
erisdinh <erisdinh@gmail.com>
2018-07-06 18:20:54 +0700

    Bus Management System Icon
    Add an icon for the program

--- BusManagementSystem/src/quynh/BusManagementSystem.java
+++ BusManagementSystem/src/quynh/BusManagementSystem.java
@@ -4,30 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class BusManagementSystem extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        
+
         // Get the controller pass the FXML file to FXMLLoader
         FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginForm.fxml"));
         Parent root = (Parent) loader.load();
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
+
+        // Set title
         stage.setTitle("Bus Management System");
-        stage.show();
-        
-        // assign stage to the controller
+
+        // Set icon
+        stage.getIcons().add(new Image(BusManagementSystem.class.getResourceAsStream("/data/bus.png")));
+
+        // Assign stage to the controller
         LoginFormController controller = (LoginFormController) loader.getController();
         controller.setStage(stage);
+        
+        stage.show();
+
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
